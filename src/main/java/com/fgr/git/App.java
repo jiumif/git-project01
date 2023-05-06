3f2e3044dc24a84f671d04c67744fcd431a1007b
jiumif <132049371+jiumif@users.noreply.github.com>
2023-05-06 16:46:48 +0800

    Update App.java

--- src/main/java/com/fgr/git/App.java
+++ src/main/java/com/fgr/git/App.java
@@ -9,7 +9,7 @@ public class App {
         System.out.println("在hotfix上面的第一次修改");
         System.out.println("在master上面的第一次修改");
         System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git 啦啦啦");
 
 
     }
