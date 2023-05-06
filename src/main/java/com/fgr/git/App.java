0f48655992e9297c028111235b53c70e59082b44
fgr <2899599952@qq.com>
2023-05-06 11:30:05 +0800

    git第一次提交

--- src/main/java/com/fgr/git/App.java
+++ src/main/java/com/fgr/git/App.java
@@ -8,6 +8,7 @@ public class App {
         System.out.println("在hotfix上面的第二次提交");
         System.out.println("在hotfix上面的第一次修改");
         System.out.println("在master上面的第一次修改");
+        System.out.println("hello git");
 
     }
 }
