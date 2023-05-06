9a8c1a402a232a2cc953fb51288cd3c91662e9e9
fgr <2899599952@qq.com>
2023-05-06 09:11:20 +0800

    在hotfix上面的第一次提交

--- src/main/java/com/fgr/git/App.java
+++ src/main/java/com/fgr/git/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
         System.out.println("第二次提交");
-
+        System.out.println("在hotfix上面的第一次提交");
     }
 }
